33ae0f0883ffdf8e7808e2ffbe7bfe5661a9290b
YilunSun <osirisnero@gmail.com>
2020-11-21 23:21:23 -0500

    test multiple exports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Button } from "./components";
+import { Button, Link } from "./components";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Button text={"hhhasdasdh"}></Button>
+        <Link text={"hhhasdasdh"}></Link>
       </div>
     );
   }
